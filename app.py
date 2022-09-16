13823a45b40d70b683d7d6e30d9be5fa0ec4943b
lukinoo <luka.bliadze.2015@mail.ru>
2022-09-16 19:17:42 +0400

    feature(header-form-styles-✨)

--- app.py
+++ app.py
@@ -1,12 +1,29 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.sql import func
 
 app = Flask(__name__)
 
 
-@app.route("/")
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.sqlite"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db = SQLAlchemy(app)
+
+
+# todo schema
+class Todo:
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(90))
+    complete = db.Column(db.Boolean)
+    created_at = db.Column(db.DateTime(timezone=True),
+                           server_default=func.now())
+
+
+@app.route("/")  # index route
 def index():
     return render_template("index.html")
 
 
 if __name__ == '__main__':
+    db.create_all()
     app.run(debug=True)
